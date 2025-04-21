a256b6072e3441a73f1bfe8f9ab89f5ef5a4e908
Nate Griepentrog <101207192+ndgriep@users.noreply.github.com>
2025-04-20 23:19:35 -0500

    delete unused files

--- grade_scale.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def compute_similarity(resume_text: str, jd_text: str) -> float:
-    vectorizer = TfidfVectorizer(stop_words='english')
-    vectors = vectorizer.fit_transform([resume_text, jd_text])
-    sim_matrix = cosine_similarity(vectors[0:1], vectors[1:2])
-    return float(sim_matrix[0][0]) * 100 
